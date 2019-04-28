a28f9834702163922de189e2752ef88dfa25cf36
Vicky <kosenchiha@gmail.com>
2019-04-28 13:22:02 +0100

    completed first part of the project

--- js/my-github.js
+++ js/my-github.js
@@ -1,2 +1,16 @@
-// Write code here to communicate with Github
-
+fetch("https://api.github.com/users/kosenchiha/repos")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(repos) {
+    document.querySelector("#repos-count").innerHTML = repos.length;
+    document.querySelector("#repos-list").innerHTML = "";
+    repos.forEach(element => {
+      var listElement = document.createElement("li");
+      document.querySelector("#repos-list").appendChild(listElement);
+      var a = document.createElement("a");
+      a.setAttribute("href", element.html_url);
+      listElement.appendChild(a);
+      a.innerHTML = element.name;
+    });
+  });
